8788f71fd21b1482ad649619007364b357247acf
Schorschyy <96734352+Schorschyy@users.noreply.github.com>
2023-01-30 16:35:26 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,19 +1,33 @@
 package Interface;
 
-public class Person {
-	public int id;
-	public double groesse;
-	public String name;
+public class Gebaeude implements Comparable<Gebaeude>{
 	
-	public Person(int id, double groesse, String name) {
-		this.id=id;
-		this.groesse=groesse;
-		this.name=name;
+	public int nummer;
+	public double hoehe;
+	public String beschreibung;
 	
+	public Gebaeude(int nummer, double hoehe, String beschreibung) {
+		this.nummer=nummer;
+		this.hoehe=hoehe;
+		this.beschreibung=beschreibung;
+	}
+	
+	
+	@Override
+	public int compareTo(Gebaeude gebaeude) {
+	if(this.hoehe==gebaeude.hoehe) {
+	return 0;
+	} else if(this.hoehe>gebaeude.hoehe) {
+	return 1;
+	} else {
+	return -1;
+	}
 	}
 	
 	@Override
 	public String toString() {
-		return "[ " + id + ", " + groesse + ", " + name + "]";
-	}
+        return "[" + nummer+ ", " + hoehe +", "+ beschreibung + "]";
+    }
+	
+
 }
